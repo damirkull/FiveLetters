8c211af5e2fdb66fbc49d719945800e3ad1c2ed2
damirkull <damir.kulaev09@gmail.com>
2026-06-30 00:57:49 +0300

    Add files via upload

---
+++ Words/Words/FormGame.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Words
+{
+    public enum Difficulty
+    {
+        Easy = 0,
+        Medium = 1,
+        Hard = 2
+    }
+    public enum Language
+    {
+        Russian = 0,
+        Ossetian = 1
+    }
+
+    public partial class FormGame : Form
+    {
+        /* 0 - легкий       русский
+         * 1 - средний      осетинский
+         * 2 - сложный */
+
+        Difficulty levelDifficulty;
+        Language language;
+        public static int cellSize = 4;
+        string[,] namesOfDifficulties =
+        {
+            { "Легкая", "Средняя", "Сложная" },
+            { "Хъæлдзæг", "Астæууон", "Зын"}
+        };
+
+        public FormGame()
+        {
+            InitializeComponent();
+            levelDifficulty = Difficulty.Medium;
+            language = Language.Russian;
+        }
+
+        private void buttonDifficulty_Click(object sender, EventArgs e)
+        {
+            if (levelDifficulty == Difficulty.Easy)
+                levelDifficulty = Difficulty.Medium;
+            else if (levelDifficulty == Difficulty.Medium)
+                levelDifficulty = Difficulty.Hard;
+            else
+                levelDifficulty = Difficulty.Easy;
+
+            buttonDifficulty.Text = namesOfDifficulties[(int)language, (int)levelDifficulty];
+        }
+
+        private void buttonPlay_Click(object sender, EventArgs e)
+        {
+            FormPlay formPlay = new FormPlay(language, levelDifficulty);
+            this.Hide();
+            formPlay.ShowDialog();
+        }
+
+        private void buttonLanguage_Click(object sender, EventArgs e)
+        {
+            if (language == Language.Russian)
+                language = Language.Ossetian;
+            else
+                language = Language.Russian;
+            ChangeLanguage(language);
+        }
+
+        private void ChangeLanguage(Language language)
+        {
+            if (language == Language.Ossetian)
+            {
+                labelName.Text = "5 Дзырды";
+                labelName.Location = new Point(66, 34);
+                labelLanguage.Text = "Æвзаг:";
+                labelDifficulty.Text = "Зынхалад:";
+                buttonPlay.Text = "Хъазын";
+                buttonLanguage.Text = "Ирон";
+                buttonDifficulty.Text = namesOfDifficulties[(int)language, (int)levelDifficulty];
+            }
+
+            else
+            {
+                labelName.Text = "5 Слов";
+                labelName.Location = new Point(111, 34);
+                labelLanguage.Text = "Язык:";
+                labelDifficulty.Text = "Сложность:";
+                buttonPlay.Text = "Играть";
+                buttonLanguage.Text = "Русский";
+                buttonDifficulty.Text = namesOfDifficulties[(int)language, (int)levelDifficulty];
+            }
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
---
+++ Words/Words/WordsRepository.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Words
+{
+    public class WordsRepository
+    {
+        List<Word> words = new List<Word>();
+         
+        public WordsRepository()
+        {
+            string[] easyRus = File.ReadAllText("words\\easyWordsRus.txt").Split(' ');
+            string[] midRus = File.ReadAllText("words\\midWordsRus.txt").Split(' ');
+            string[] hardRus = File.ReadAllText("words\\hardWordsRus.txt").Split(' ');
+            string[] easyRusTrans = File.ReadAllText("words\\easyWordsRusTranslate.txt").Split(' ');
+            string[] midRusTrans = File.ReadAllText("words\\midWordsRusTranslate.txt").Split(' ');
+            string[] hardRusTrans = File.ReadAllText("words\\hardWordsRusTranslate.txt").Split(' ');
+
+            string[] easyOss = File.ReadAllText("words\\easyWords.txt").Split(' ');
+            string[] midOss = File.ReadAllText("words\\midWords.txt").Split(' ');
+            string[] hardOss = File.ReadAllText("words\\hardWords.txt").Split(' ');
+            string[] easyOssTrans = File.ReadAllText("words\\easyWordsTranslate.txt").Split(' ');
+            string[] midOssTrans = File.ReadAllText("words\\midWordsTranslate.txt").Split(' ');
+            string[] hardOssTrans = File.ReadAllText("words\\hardWordsTranslate.txt").Split(' ');
+
+            AddWords(easyRus, easyRusTrans, Difficulty.Easy, Language.Russian);
+            AddWords(midRus, midRusTrans, Difficulty.Medium, Language.Russian);
+            AddWords(hardRus, hardRusTrans, Difficulty.Hard, Language.Russian);
+            AddWords(easyOss, easyOssTrans, Difficulty.Easy, Language.Ossetian);
+            AddWords(midOss, midOssTrans, Difficulty.Medium, Language.Ossetian);
+            AddWords(hardOss, hardOssTrans, Difficulty.Hard, Language.Ossetian);
+        }
+
+        void AddWords(string[] texts, string[] translates, Difficulty difficulty, Language language)
+        {
+            int count = texts.Length;
+
+            for (int i = 0; i < count; i++)
+                words.Add(new Word(texts[i], translates[i], difficulty, language));
+        }
+
+        public Word GetRandomWord(Difficulty difficulty, Language language)
+        {
+            List<Word> newWords = new List<Word>();
+
+            foreach (Word word in words)
+            {
+                if (word.Difficulty == difficulty && word.Language == language)
+                    newWords.Add(word);
+            }
+
+            Random rand = new Random();
+            int randIndex = rand.Next(newWords.Count);
+            return newWords[randIndex];
+        }
+    }
+}
